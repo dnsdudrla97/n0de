1f8eaebdd881a0169f72c49aeb181d1d0f9f956e
dnsdudrla97 <dnsdudrla97@naver.com>
2020-08-26 11:40:30 +0900

    server on

--- zero/server.js
+++ zero/server.js
@@ -1,24 +1,30 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
 var app = http.createServer(function(request, response) {
-    var url = request.url;
-    // console.log(url)
-    if (url == '/')  {
-        url = '/index.html';
+    var _url = request.url;
+    var query = url.parse(_url, true).query; //GET
+
+    // console.log(_url)
+    if (_url == '/')  {
+        _url = '/index.html';
         // console.log(__dirname);
     }
-    if (url == '/favicon.ico') {
+    if (_url == '/favicon.ico') {
         response.writeHead(404);
         response.end();
         return;
 
     }
     response.writeHead(200);
-    console.log(__dirname+ url);
+    console.log(__dirname+ _url);
     // console.log(process.env.PATH);
-    
-    response.end(fs.readFileSync( __dirname+'/'+url));
+
+    if(query.name)
+        response.end(fs.readFileSync( __dirname+'/'+query.name));
+    else
+        response.end(fs.readFileSync( __dirname+'/'+_url));
     //  readFileSync : 경로에 해당 하는 파일을 읽어 가져온다.
     // response.end : 사용자에게 전송할 데이터 생성
 });
